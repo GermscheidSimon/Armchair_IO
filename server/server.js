bb6701b5158467cc2e8d1eeef104f023f2ee9eeb
nrminar <nrminar@gmail.com>
2021-07-01 17:25:02 -0500

    change boot route

--- server/server.js
+++ server/server.js
@@ -10,12 +10,11 @@ const PORT = process.env.PORT || 5000;
 // Route includes
 const bootRouter = require('./routes/boot.router');
 
+/* Routes */
 app.get('/ping',  (req, res) => {
     res.send('alive')
 });
-
-/* Routes */
-app.use('/boot', bootRouter);
+app.get('/boot', bootRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
