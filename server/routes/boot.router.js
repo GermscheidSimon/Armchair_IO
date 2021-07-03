414453fe2e5fea10562584f5894889e94712cfbc
nrminar <nrminar@gmail.com>
2021-07-03 13:05:58 -0500

    create new turtle

--- server/routes/boot.router.js
+++ server/routes/boot.router.js
@@ -3,12 +3,13 @@ const generator = require('../modules/generator');
 const jobMap = require('../luascripts/index')
 const ramDb = require('../dataAccess/ramDB')
 const fsOps = require('../modules/fsOps/fsOps')
+const Turtle = require('../Models/Turtle')
 const router = express.Router();
 
 
 router.get('/', (req, res) =>{
     let newGuid = generator.guid()
-    let newTurtle = ramDb.create({label: newGuid, watIsPurpose: 'mine'})
+    let newTurtle = new Turtle(label = newGuid, watIsPurpose ='mine')
     let jobScript = fsOps.readFile(jobMap[newTurtle.watIsPurpose])
     newTurtle.jobScript = jobScript
     res.send(JSON.stringify(newTurtle))
