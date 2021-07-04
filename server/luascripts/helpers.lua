c64e29a4ee96740fe945243af57b6d5646c351d6
Simon Germscheid <67908776+Khaildoran@users.noreply.github.com>
2021-07-03 22:51:32 -0500

    negative equality is ! !

--- server/luascripts/helpers.lua
+++ server/luascripts/helpers.lua
@@ -3,7 +3,7 @@ function tableToJson(tableObj)
     local jsonStr = "{"
 
     for k, v in pairs(tableObj) do
-        if  k != nil then
+        if  k ~= nil then
             jsonStr = jsonStr + k ":" + v ","
         end
     end
