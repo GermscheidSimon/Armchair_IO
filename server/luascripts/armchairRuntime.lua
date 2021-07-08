21a15f1c9c08adc6abe03326fb5f8912ba538a93
Simon Germscheid <67908776+Khaildoran@users.noreply.github.com>
2021-07-08 08:59:00 -0500

    created recursive setup for runtime

--- server/luascripts/armchairRuntime.lua
+++ server/luascripts/armchairRuntime.lua
@@ -24,7 +24,7 @@ local OrchestrationCodeMap = {
     exe_Puprose        - 1 - Executing Purpose
     exe_WebSocketState - 2 - Controlled from Server via WebSocket
 
-    err_runtime        - 3 - Failed to Execute Runtime operation
+    err_runtime        - 3 - Failed
     err_pupose         - 4 - Exception Thrown in Purpose Script
     err_server         - 5 - Connection or server error exception
     err_gps            - 7 - unable to handle GPS location/vectoring
@@ -37,22 +37,32 @@ local originStateBuilder = function()
     originx, originy, originy = gps.locate()
     originFuelLVl = turtle.getFuelLevel()
     originLavel = os.getComputerLabel()
-
-
-    return {
-        Location = {
-            x = originx,
-            y = originy,
-            z = originz
-        },
-        FuelLvl = originFuelLvl,
-        Purpose = "origin",
-        Label = originLabel,
-        errorState = 0
-    }
+    if 
+        originx ~= 0 and
+        originx ~= 0 and
+        originx ~= 0 
+    then
+        StateModel = {
+            Location = {
+                x = originx,
+                y = originy,
+                z = originz
+            },
+            FuelLvl = originFuelLvl,
+            Purpose = "origin",
+            Label = originLabel,
+            errorState = 0
+        }
+        print('origin state created')
+        return true
+    else
+        print('failed to initialize origin state')
+        return false
+    end
 end
 
 local postState = function(stateModel)
+    print('posting Origin to host')
     stateModelJson = helpers.tableToJson(tableToJson)
 
     return http.post("https://armchair-io.herokuapp.com/boot/initializeTurtle", stateModelJson)
@@ -62,6 +72,7 @@ local createOriginstateFile = function(state)
     orgiinFile = fs.open('originConfig.lua','w')
     originFile.write(state)
     originFile.close()
+
 end
 
 local assignComputerLabel = function(state)
@@ -69,8 +80,10 @@ local assignComputerLabel = function(state)
        os.setComputerLabel(state.Label)
     end
     if os.getComputerLabel == state.Label then
+        print('computer label assignment successful')
         return true
     else 
+        print('computer label assignment failed')
         return false
     end
 end
@@ -78,22 +91,55 @@ end
 
 local runtime = function()
 
-    print(originState)
-    originState = originStateBuilder()
-    createOriginstateFile(originState)
-    originPostStats = postState(originState)
     
 
-    if originPostStats ~= nil then
-        nextState = textutils.unserialiseJSON(originPostStats)
-        print(nextState)
+    if StateModel[errorState] == OrchestrationCodeMap[ready] then
 
-        StateModel = nextState
+        --build origin state
+                print("initializing Runtime")
+                print("building Turtle Origin State")
+        --
+        originStateIsCreated = originStateBuilder()
+        computerLabelCreated = false
+        if originStateIsCreated then
+            createOriginstateFile()
+            --
+                print("origin file created")
 
+            originPostStats = postState(originState)
+        
+            if originPostStats ~= nil then
+                    print('purpose assigned')
+                nextState = textutils.unserialiseJSON(originPostStats)
+                    print(nextState)
+                StateModel = nextState
+                computerLabelCreated =  assignComputerLabel()
+            end
+        end
 
+        if computerLabelCreated then
+                print('initial runtime successful')
+            runtime()
+        else
+                print('error in runtime')
+            StateModel[errState] = OrchestrationCodeMap[err_runtime]
+            runtime()
+        end
+        
+    else if StateModel[errorState] == OrchestrationCodeMap[exe_Purpose] then
+                print('executing purpose script')
+        -- pCall() purpose script
+        -- then runtime()
 
-
+    else if StateModel[errorState] == OrchestrationCodeMap[exe_WebSocketState] then
+                print('connecting to host websocket')
+        -- pCall() websocketScript
+        -- then runtime()
+    else 
+        -- Handle Error 
+        --then runtime()
     end
+
 end
 
 runtime()
